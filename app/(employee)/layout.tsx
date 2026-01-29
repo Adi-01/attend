71c1a3968cc46ca16af2ef19e1b84f5e11c9e0a0
Aditya <pushpamaditya@gmail.com>
2026-01-29 21:47:23 +0530

    moved app to website 2

--- app/(employee)/layout.tsx
+++ app/(employee)/layout.tsx
@@ -3,9 +3,13 @@ import { cookies } from "next/headers";
 import Providers from "@/components/providers";
 import { AuthNavbar } from "@/components/attendance/AttendanceNavbar";
 
-const cookie = await cookies();
-const role = cookie.get("user-label")?.value ?? "";
-export default function BaseLayout({ children }: { children: ReactNode }) {
+export default async function BaseLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const cookie = await cookies();
+  const role = cookie.get("user-label")?.value ?? "";
   return (
     <Providers>
       <AuthNavbar role={role} />
--- app/(employee)/mark-attendance/page.tsx
+++ app/(employee)/mark-attendance/page.tsx
@@ -1,10 +1,9 @@
 import AttendanceContainer from "@/components/attendance/AttendanceContainer";
 import { cookies } from "next/headers";
 
-const cookie = await cookies();
-const username = cookie.get("username")?.value ?? "";
-
 export default async function MarkAttendancePage() {
+  const cookie = await cookies();
+  const username = cookie.get("username")?.value ?? "";
   return (
     <main className="min-h-screen bg-black/95 py-10 px-4 flex justify-center mt-20">
       <div className="w-full max-w-xl space-y-6">
